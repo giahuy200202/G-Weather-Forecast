1408159725adea67268e7381c168f6b82bb2b458
GiaHuy <74665412+giahuy200202@users.noreply.github.com>
2024-06-11 09:26:57 +0700

    weather ui banner

--- frontend/src/components/Dashboard/Result/index.tsx
+++ frontend/src/components/Dashboard/Result/index.tsx
@@ -3,16 +3,41 @@ import styles from "./result.module.css";
 import { useAppDispatch, useAppSelector } from "../../../hooks/ReduxHooks";
 import BeatLoader from "react-spinners/BeatLoader";
 import toast, { Toaster } from "react-hot-toast";
+import { ReactComponent as EmailIcon } from "../../../assets/svg/email.svg";
+
 import { dashboardActions } from "@store/dashboard";
+import { useState } from "react";
 
 const Result: React.FC = () => {
-  // const tasklistData = useAppSelector((state) => state.dashboard.tasklistData);
   const dispatch = useAppDispatch();
 
-
   return (
-    <div className={styles["tasklist-container"]}>
-      
+    <div className={styles["result-container"]}>
+      <div className={styles["banner-container"]}>
+        <div>
+          <h1>London (2023-06-19)</h1>
+          <p>Temparature: 18.17 °C</p>
+          <p>Wind: 18.17 M/S</p>
+          <p>Humidity: 76 %</p>
+        </div>
+        <div className={styles["condition-container"]}>
+          <img src="https://cdn.weatherapi.com/weather/64x64/day/176.png" alt="weather icon" />
+          <p>Party cloudy</p>
+        </div>
+      </div>
+      <h1>4-Day Forecast</h1>
+      <div className={styles["banner-container"]}>
+        <div>
+          <h1>London (2023-06-19)</h1>
+          <p>Temparature: 18.17 °C</p>
+          <p>Wind: 18.17 M/S</p>
+          <p>Humidity: 76 %</p>
+        </div>
+        <div className={styles["condition-container"]}>
+          <img src="https://cdn.weatherapi.com/weather/64x64/day/176.png" alt="weather icon" />
+          <p>Party cloudy</p>
+        </div>
+      </div>
     </div>
   );
 };
